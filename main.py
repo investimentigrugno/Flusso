9121a84bab7667f86ac3e12121ac43c007d9c0b1
investimentigrugno <investimentigrugno@gmail.com>
2025-10-14 10:41:14 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+# main.py
+import streamlit as st
+from stock_screener import stock_screener_app
+from password_decryptor import password_decryptor_app
+
+st.set_page_config(page_title="Multi Utility App", layout="wide")
+
+MENU = {
+    "Stock Screener": stock_screener_app,
+    "CSV Password Decryptor": password_decryptor_app,
+    # Potrai aggiungere qui future funzionalità:
+    # "Nuova Dashboard": nuova_dashboard_app,
+}
+
+st.sidebar.title("Menu")
+scelta = st.sidebar.radio("Seleziona funzionalità", list(MENU.keys()))
+
+# Qui viene eseguito il modulo scelto
+MENU[scelta]()
