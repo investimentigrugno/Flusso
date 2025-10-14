58276c1269d03259486f095ce65c3d166e5261f4
investimentigrugno <investimentigrugno@gmail.com>
2025-10-15 00:20:47 +0200

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -24,15 +24,11 @@ def portfolio_tracker_app():
             df_filtered = df.iloc[:16, :13]
             
            # Prendi righe 19-21 (inclusi) saltando la vuota (riga 18 vuota)
-            df_summary = df.iloc[19:21, 3:12].copy()  # righe 20 e 21 nel foglio, quindi indici 19 e 20 in python
+            df_summary = df.iloc[17:19, 3:12].copy()  # righe 20 e 21 nel foglio, quindi indici 19 e 20 in python
             
-            # Imposta la riga 20 (indice 19 nel dataframe) come intestazione
-            df_summary.columns = df_summary.iloc[0].str.strip()
-            
-            # Rimuovi la riga intestazione per mostrare solo i dati effettivi (riga 21)
-            df_summary = df_summary.iloc[1:].reset_index(drop=True)
+            header_names = ["DEPOSIT", "VALUE €", "P&L %", "P&L TOT", "P&L %", "P&L LIVE", "TOT $", "EUR/USD", "TOT €"]
+			df_summary.columns = header_names
 
-            
         st.success("✅ Dati caricati con successo!")
         
         # Visualizza tabella riepilogativa principale
